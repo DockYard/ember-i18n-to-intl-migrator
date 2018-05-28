6dc98337ea72aed6625b10d050cb31f01cb73d90
cibernox <miguel.camba@gmail.com>
2018-05-28 16:42:47 +0200

    Remove unneeded file, add warning about old files.

--- bin/ember-i18n-intl-migrator.js
+++ bin/ember-i18n-intl-migrator.js
@@ -8,6 +8,7 @@ const translationTransform = require('../lib/translation-transform');
 
 try {
   translationTransform(argv.type);
+  console.log(chalk.yellow("Your new translations have been stored in /translations, but your old translation files have not been removed. Please review the result and delete the old translation files if satisfied."));
 } catch (e) {
   console.error(chalk.red(e.stack));
   process.exit(-1);
