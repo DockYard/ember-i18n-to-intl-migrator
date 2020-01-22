b06ed29899ee894738fff5306ffec11fd2db7959
Jeldrik Hanschke <admin@jhanschke.de>
2020-01-22 22:45:18 +0100

    support dot notation in nested object

--- dummy/app/locales/de/translations.js
+++ dummy/app/locales/de/translations.js
@@ -1,4 +1,8 @@
 export default {
+  button: {
+    'state.loading': 'Loading...',
+    'state.loaded': 'Loaded',
+  },
   'user.edit.title': 'Edit User',
   'user.followers.title.one': 'One Follower',
   'user.followers.title.other': 'All {{count}} Followers',
